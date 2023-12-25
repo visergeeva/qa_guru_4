74ab5986205396d42d8b7c16d7331e24e9d2d536
V.Sergeeva <tishchkan@rambler.ru>
2023-12-25 22:00:20 +0300

    new commit

--- src/test/java/guru/qa/SimpleJUnitTest.java
+++ src/test/java/guru/qa/SimpleJUnitTest.java
@@ -1,12 +1,36 @@
 package guru.qa;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class SimpleJUnitTest {
 
+    @BeforeAll
+    static void setUp() {
+        System.out.println("_______setUp()");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("_______beforeEach");
+    }
+
     @Test
-        void assertTest() {
-            Assertions.assertTrue(2 < 3);
-        }
+    void firstTest() {
+        System.out.println("_______firstTest");
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("_______secondTest");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("_______afterEach");
+    }
+
+    @AfterAll
+    static void tearDown() {
+        System.out.println("_______tearDown()");
+    }
 }
